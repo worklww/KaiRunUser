6b0a043ed52018124a65a64e93fef2ff1bd988c9
worklww <workwwl@126.com>
2016-03-23 09:56:13 +0800

    zz

--- app/src/main/java/cn/kairun/kairunuser/MainActivity.java
+++ app/src/main/java/cn/kairun/kairunuser/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity{
-
+//11
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
