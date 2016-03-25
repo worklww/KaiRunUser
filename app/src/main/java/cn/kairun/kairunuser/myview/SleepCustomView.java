d2d54a7f529a269a0c6f058243fd092f82c2dbbd
ZengRong <258150546@qq.com>
2016-03-25 16:36:05 +0800

    自定义view

---
+++ app/src/main/java/cn/kairun/kairunuser/myview/SleepCustomView.java
@@ -0,0 +1,7 @@
+package cn.kairun.kairunuser.myview;
+
+/**
+ * Created by ZengRong on 2016/3/25.
+ */
+public class SleepCustomView {
+}
--- app/src/main/java/cn/kairun/kairunuser/myview/SportCustomView.java
+++ app/src/main/java/cn/kairun/kairunuser/myview/SportCustomView.java
@@ -4,7 +4,9 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.View;
@@ -24,6 +26,22 @@ public class SportCustomView extends View {
      * 圈的宽度
      */
     private int mCircleWidth;
+
+    /**
+     * 内环第一圈的颜色
+     */
+    private int insidefirstColor;
+
+    /**
+     * 内环第二圈的颜色
+     */
+    private  int insidesecondColor;
+
+    /**
+     * 内环圈的宽度
+     */
+    private int insidemCircleWidth;
+
     /**
      * 画笔
      */
@@ -32,7 +50,6 @@ public class SportCustomView extends View {
      * 当前进度
      */
     private int mProgress;
-
     /**
      * 速度
      */
@@ -42,6 +59,13 @@ public class SportCustomView extends View {
      * 是否应该开始下一个
      */
     private boolean isNext = false;
+    private boolean insideIsNext = false;
+
+    /**
+     * 步数和卡路里
+     */
+    private int stepCount = 0;
+    private int calorieCount = 0;
 
     public SportCustomView(Context context, AttributeSet attrs)
     {
@@ -60,8 +84,7 @@ public class SportCustomView extends View {
      * @param attrs
      * @param defStyle
      */
-    public SportCustomView(Context context, AttributeSet attrs, int defStyle)
-    {
+    public SportCustomView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomProgressBar, defStyle, 0);
         int n = a.getIndexCount();
@@ -84,30 +107,51 @@ public class SportCustomView extends View {
                 case R.styleable.CustomProgressBar_speed:
                     mSpeed = a.getInt(attr, 20);// 默认20
                     break;
+
+                case R.styleable.CustomProgressBar_insidefirstColor:
+                    insidefirstColor = a.getColor(attr,Color.GREEN);
+                    break;
+                case R.styleable.CustomProgressBar_insidesecondColor:
+                    insidesecondColor = a.getColor(attr, Color.RED);
+                    break;
+                case R.styleable.CustomProgressBar_insidecircleWidth:
+                    insidemCircleWidth = a.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_PX, 20, getResources().getDisplayMetrics()));
+                    break;
             }
         }
-        a.recycle();
-        mPaint = new Paint();
+        a.recycle(); //回收资源
+        mPaint = new Paint(); //初始化画笔
         // 绘图线程
-        new Thread()
-        {
+        new Thread() {
             public void run()
             {
                 while (true)
                 {
                     mProgress++;
+                    stepCount++;
+                    calorieCount++;
                     if (mProgress == 360)
                     {
                         mProgress = 0;
-                        if (!isNext)
+                        stepCount = 0;
+                        calorieCount = 0;
+                        if (!isNext) {
                             isNext = true;
-                        else
-                            isNext = false;
+                        } else {
+                            isNext = true;
+                        }
+
+                        if(!insideIsNext){
+                            insideIsNext = true;
+                        } else {
+                            insideIsNext = true;
+                        }
                     }
                     postInvalidate(); //重绘圆
                     try
                     {
-                        Thread.sleep(mSpeed);
+                        Thread.sleep(mSpeed); //绘制速度
                     } catch (InterruptedException e)
                     {
                         e.printStackTrace();
@@ -115,31 +159,65 @@ public class SportCustomView extends View {
                 }
             };
         }.start();
-
-    }
+   }
 
     @Override
-    protected void onDraw(Canvas canvas)
-    {
-
+    protected void onDraw(Canvas canvas) {
+        /**
+         * 内圆
+         */
         int centre = getWidth() / 2; // 获取圆心的x坐标
-        int radius = centre - mCircleWidth / 2;// 半径
+        int radius = 300;// 半径
         mPaint.setStrokeWidth(mCircleWidth); // 设置圆环的宽度
         mPaint.setAntiAlias(true); // 消除锯齿
         mPaint.setStyle(Paint.Style.STROKE); // 设置空心
         RectF oval = new RectF(centre - radius, centre - radius, centre + radius, centre + radius); // 用于定义的圆弧的形状和大小的界限
-        if (!isNext)
-        {// 第一颜色的圈完整，第二颜色跑
-            mPaint.setColor(mFirstColor); // 设置圆环的颜色
-            canvas.drawCircle(centre, centre, radius, mPaint); // 画出圆环
-            mPaint.setColor(mSecondColor); // 设置圆环的颜色
-            canvas.drawArc(oval, -90, mProgress, false, mPaint); // 根据进度画圆弧
-        } else
-        {
-            mPaint.setColor(mSecondColor); // 设置圆环的颜色
-            canvas.drawCircle(centre, centre, radius, mPaint); // 画出圆环
-            mPaint.setColor(mFirstColor); // 设置圆环的颜色
-            canvas.drawArc(oval, -90, mProgress, false, mPaint); // 根据进度画圆弧
-        }
+        mPaint.setColor(mSecondColor); // 设置圆环的颜色
+        canvas.drawCircle(centre, centre, radius, mPaint); // 画出圆环
+        mPaint.setColor(mFirstColor); // 设置圆环的颜色
+        canvas.drawArc(oval, -90, mProgress, false, mPaint); // 根据进度画圆弧
+
+
+        /**
+         * 外圆
+         */
+        int indiseCentre = getWidth() / 2; // 获取圆心的x坐标
+        int indiseRadius = 250;// 半径
+        mPaint.setStrokeWidth(insidemCircleWidth); // 设置圆环的宽度
+        mPaint.setAntiAlias(true); // 消除锯齿
+        mPaint.setStyle(Paint.Style.STROKE); // 设置空心
+        RectF indiseOval = new RectF(indiseCentre - indiseRadius, indiseCentre - indiseRadius, indiseCentre + indiseRadius, indiseCentre + indiseRadius); // 用于定义的圆弧的形状和大小的界限
+        mPaint.setColor(insidesecondColor); // 设置圆环的颜色
+        canvas.drawCircle(indiseCentre, indiseCentre, indiseRadius, mPaint); // 画出圆环
+        mPaint.setColor(insidefirstColor); // 设置圆环的颜色
+        canvas.drawArc(indiseOval, -90, mProgress, false, mPaint); // 根据进度画圆弧
+
+        mPaint = null;
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStrokeWidth(3);
+        mPaint.setTextSize(80);
+        String testString = "运动";
+        mPaint.setColor(ContextCompat.getColor(getContext(), R.color.colortextcolor));
+        mPaint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(testString, indiseOval.centerX(), indiseOval.centerX() - 85, mPaint);
+
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStrokeWidth(2);
+        mPaint.setTextSize(80);
+        mPaint.setColor(ContextCompat.getColor(getContext(), R.color.colorabroadcircular));
+        mPaint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(String.valueOf(stepCount)+"步", indiseOval.centerX(), indiseOval.centerX() + 20, mPaint);
+
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStrokeWidth(2);
+        mPaint.setColor(ContextCompat.getColor(getContext(), R.color.colortextcolor));
+        canvas.drawLine(indiseOval.centerX() - 200, indiseOval.centerY() + 50, indiseOval.centerX() + 200, indiseOval.centerY() + 50, mPaint);
+
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setStrokeWidth(2);
+        mPaint.setTextSize(50);
+        mPaint.setColor(ContextCompat.getColor(getContext(), R.color.colorBlue));
+        mPaint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(String.valueOf(calorieCount)+"卡", indiseOval.centerX(), indiseOval.centerX() + 120, mPaint);
     }
 }
